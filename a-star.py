b3a607a7aa02164b5a020ad21e02690d9578393d
jteano <36381330+jteano@users.noreply.github.com>
2022-09-01 12:02:41 -0700

    Added map.txt and coordinates.txt

--- a-star.py
+++ a-star.py
@@ -1,12 +1,18 @@
+import sys
 import math
 
 # Class to create a city object which has name, longitude and latitude as attributes
+# adjacent_cities is a list of tuples that contain adjacent city name and distance
 class City:
-    def __init__(self, name, longitude, latitude):
+    def __init__(self, name, latitude, longitude):
         self.name = name
-        self.longitude = longitude
-        self.latitude = latitude
+        self.latitude = math.radians(latitude)
+        self.longitude = math.radians(longitude)
+        self.adjacent_cities = []
 
+    def add_adjacent_city(self, adjacent_city_name, distance_in_miles):
+        self.adjacent_cities.append((adjacent_city_name, distance_in_miles))
+        
 # Function to take departing city and arriving city (as input arguments)
 
 # Function to compute the straight line distance using the Haversine formula
@@ -14,10 +20,19 @@ class City:
 def straight_line_distance(city_1, city_2):
     # Radius of the earth
     r = 3958.8
-
+    d = 2 * r * math.asin(math.sqrt((math.sin((city_2.latitude - city_1.latitude) / 2) ** 2) + math.cos(city_1.latitude) * math.cos(city_2.latitude) * math.sin((city_2.longitude - city_1.longitude) / 2) ** 2))
+    return d
 
 # Function to convert degrees to radians
 def degrees_to_radians(degrees):
     return math.radians(degrees)
 
-# Function to parse coordinates.txt and map.txt and store in a lookup table
+# Function to parse coordinates.txt and map.txt
+
+
+LongBeach = City("LongBeach", 33.77162218, -118.1838742)
+SanJose = City("SanJose", 37.38305013, -121.8734782)
+
+LongBeach.add_adjacent_city("LosAngeles", 24.1)
+
+print(LongBeach.adjacent_cities)
